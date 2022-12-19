e0f7abbaaa8fed7a4bc9f5130226c66092754190
luisao8 <74673031+luisao8@users.noreply.github.com>
2022-12-19 11:00:06 +0100

    Del termindo d vrdad y sin page reload. Edit casi.

--- App.js
+++ App.js
@@ -11,11 +11,19 @@ const usersFetch = fetch('http://localhost:3000/users');
 const commentsFetch = fetch('http://localhost:3000/comments');
 let buttonID = document.querySelector('#main-body');
 buttonID.addEventListener('click', deleteFuncion);
+
+
 const deletePost = {
     method: 'DELETE'};
 const updatePost = {
     method: 'POST'};
 let url = 'http://localhost:3000/posts/';
+
+
+ 
+
+
+
 
 async function fetcData() {
     const fetchData= await Promise.all([postsFetch, usersFetch, commentsFetch]).then(values => {
@@ -36,36 +44,113 @@ function numLoad(indexChange,numShowPosts) {
         const {name : commentName, body : commentBody, email : commentMail} = comments[j];
         body.append(createHTML(title, postBody,id, userMail, userUsername, commentName, commentBody, commentMail));
         z = z + 1;
-        j = j + 1;        
+        j = j + 1;      
     }
 }
 
 window.onload = fetcData();
-loadMore1.addEventListener("click" ,load) 
-function load() {
-        indexChange = indexChange+4
-        numShowPosts = numShowPosts+4
-}    
+// loadMore1.addEventListener("click" ,load) 
+// function load() {
+//         indexChange = indexChange+4
+//         numShowPosts = numShowPosts+4
+// }
+
+
+function postStyleDelete(e) {
+    console.log(e)
+    let target = e.closest('main > div');
+    target.style.display = 'none';
+    };    
+    
+    
+
+
 
 function deleteFuncion (e) {
     if (!e) {e=window.event}
     e.preventDefault();
-    let edit = "edit"
-    let save = "save"
-    let idcontainer = e.target["id"]
-    if (idcontainer != edit) {
-        let urlid = url+idcontainer
+    let targetButt = e.target;
+    let div33 = targetButt;
+    targetButt = targetButt.className;
+    let classNameButt = 'buttomRemove btn btn-outline-danger btn-sm me-md-2';
+    let idcontainer = e.target["id"];
+    if (targetButt == classNameButt) {
+        let urlid = url+idcontainer;
+        urlid = urlid.split('');
+        urlid = urlid.filter(word => word.length < 31);
+        urlid = urlid.join('');
         fetch(urlid,deletePost)
-        .then(function() {
-        }).catch(error => console.log(error));
-    }
-    if (idcontainer === save) {
-        console.log("hola");
+        postStyleDelete(div33);
     }
 };
-function editPost() {
-console.log(valor);
-}
+
+// buttonID.addEventListener('click', findEditID);
+// function findEditID(e) {
+//     if (e.target["id"] == 'edit') {
+//         console.log('hola')
+//         let target = e.target;
+//         target = target.closest('main > div');
+//         console.log(target)
+//     };
+// };
+
+
+let titleContent = '';
+let bodyContent = '';
+let urlEdit = '';
+let careful = '';
+
+buttonID.addEventListener('click', findSaveID);
+function findSaveID(e) {
+    careful = e.target['id'];
+    careful = careful.substring(4,6);
+    careful = careful.toString();
+    localStorage.setItem("id", careful);
+    console.log(careful)
+};
+
+buttonID.addEventListener('click', updateInfo);
+function updateInfo(e) {
+    e.preventDefault()
+    if (e.target["id"] == 'save') {
+        e.preventDefault()
+        console.log('////////////////////////////////////////////')
+//         import { idBorrow } from "./module2";
+        let idRet = sessionStorage.getItem("id");
+        console.log(idBorrow)
+        let textContent = 'title' + idRet;
+        console.log(idRet)
+        console.log(textContent)
+        textContent = document.querySelector(`[aria-label='${textContent}']`);
+        titleContent = textContent.value;
+        let bodyContent = 'content' + careful;
+        console.log(bodyContent)
+        bodyContent = document.querySelector(`[aria-label='${bodyContent}']`);
+        bodyContent = bodyContent.value;
+        urlEdit = '';
+        urlEdit = url + idButts;
+        console.log(urlEdit)
+        fetch(urlEdit).then(response => response.json()).then((response) => {
+            updatePostFunc(response, titleContent, bodyContent);
+        });   
+    };
+};
+
+// function updatePostFunc (response, titleContent, bodyContent) {
+//     response.title = titleContent;
+//     response.body = bodyContent;
+//     console.log(response)
+//     console.log(urlEdit)
+//     response = JSON.stringify(response)
+//     console.log(response)
+//     let option = {
+//         method: 'PUT',
+//         body: response
+//     }
+//     console.log(response)
+//     fetch(urlEdit, option).then(response => console.log(response.status))
+// }
+
 
 function createHTML (title, postBody,id, userMail, userUsername, commentName, commentBody, commentMail) {
     let main = document.createElement('div');
@@ -79,7 +164,7 @@ function createHTML (title, postBody,id, userMail, userUsername, commentName, co
                     <button type="button" class="btn btn-Warning" data-bs-toggle="modal" data-bs-target="#modal-${id}">Show Post</button>
                 </div>
                 <div class="border-bottom d-md-flex justify-content-md-end">
-                <button type="button" class="buttomEdit btn-primary" id="edit" btn btn-outline-primary btn-sm me-md-2" data-bs-toggle="modal" data-bs-target="#ventanaModalEdit" >Edit</button>
+                <button type="button" class="buttomEdit btn-primary" id="edit${id}" btn btn-outline-primary btn-sm me-md-2" data-bs-toggle="modal" data-bs-target="#ventanaModalEdit" >Edit</button>
                 <button type="button" id="${id}" class="buttomRemove btn btn-outline-danger btn-sm me-md-2">Remove</button>
                 </div>
             </div>
@@ -123,9 +208,9 @@ function createHTML (title, postBody,id, userMail, userUsername, commentName, co
             </div>
             <div class="modal-body">
             <h4>Tittle</h4>
-            <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+            <input type="text" class="form-control" aria-label="title${id}" aria-describedby="basic-addon1">
             <h4>Content</h4>
-            <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+            <input type="text" class="form-control" aria-label="content${id}" aria-describedby="basic-addon1">
             </div>
             <div class="modal-footer">
                 <button type="button" id="save" class="btn btn-primary">Save changes</button>
